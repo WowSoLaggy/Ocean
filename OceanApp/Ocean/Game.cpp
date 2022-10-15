5d528a46d91b2bc378619ce59b87b9497ab53a29
WowSoLaggy <dohxehapo@gmail.com>
2022-10-15 21:38:09 +0500

    (+) Added camera and controller

--- OceanApp/Ocean/Game.cpp
+++ OceanApp/Ocean/Game.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "Game.h"
 
+#include <LaggyDx/FirstPersonController.h>
 #include <LaggyDx/GameSettings.h>
 
 
@@ -10,6 +11,8 @@ namespace
   {
     static Dx::GameSettings settings;
     settings.applicationName = "Ocean Sim";
+    settings.screenWidth = 1024;
+    settings.screenHeight = 768;
     return settings;
   }
 
@@ -20,4 +23,25 @@ Game::Game()
   : Dx::Game(getGameSettings())
 {
   getInputDevice().showCursor();
+
+  d_camera = Dx::ICamera::createFirstPersonCamera(
+    { getGameSettings().screenWidth, getGameSettings().screenHeight });
+  d_camera->setPosition({ -10.0f, 10.0f, -10.0f });
+  d_camera->setLookAt({ 0.0f, 0.0f, 0.0f });
+
+  d_inputController = std::make_unique<Dx::FirstPersonController>(*this, *d_camera);
+void Game::update(double i_dt)
+{
+  auto toStr = [](const auto& i_vec) -> std::string
+  {
+    return std::to_string(i_vec.x) + ", " + std::to_string(i_vec.y) + ", " + std::to_string(i_vec.z);
+  };
+
+  Dx::Game::update(i_dt);
+}
+
+
+void Game::render()
+{
+  Dx::Game::render();
 }
--- OceanApp/Ocean/Game.h
+++ OceanApp/Ocean/Game.h
@@ -1,10 +1,20 @@
 #pragma once
 
 #include <LaggyDx/Game.h>
+#include <LaggyDx/ICamera.h>
+#include <LaggyDx/IInputController.h>
 
 
 class Game : public Dx::Game
 {
 public:
   Game();
+
+  virtual void update(double i_dt) override;
+  virtual void render() override;
+
+private:
+  std::unique_ptr<Dx::ICamera> d_camera;
+
+  std::unique_ptr<Dx::IInputController> d_inputController;
 };
